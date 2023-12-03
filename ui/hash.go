8d21de34c61ffd2caf0910e9a9eae8b6ea7c2a2f
Josh Powers <powersj@fastmail.com>
2023-12-03 16:06:45 -0700

    feat: example working buttons and on click

--- ui/hash.go
+++ ui/hash.go
@@ -1,21 +1,32 @@
 package ui
 
-import "github.com/AllenDang/giu"
+import (
+	"devdex"
+
+	"github.com/AllenDang/giu"
+)
 
 var (
-	hashInput string
+	hashInput      string
+	hashMD5        string
+	hashSHA256     string
+	hashBlake2b512 string
 )
 
 func Hash() giu.Layout {
 	return giu.Layout{
 		giu.Label("Hash Layout"),
 		giu.Label("Input"),
-		giu.InputText(&hashInput),
+		giu.InputText(&hashInput).OnChange(func() {
+			hashMD5 = devdex.MD5Sum(hashInput)
+			hashSHA256 = devdex.SHA256Sum(hashInput)
+			hashBlake2b512 = devdex.Blake2b512Sum(hashInput)
+		}),
 		giu.Label("MD5"),
-		giu.InputText(&jsonOutput),
+		giu.InputText(&hashMD5),
 		giu.Label("SHA256"),
-		giu.InputText(&jsonOutput),
+		giu.InputText(&hashSHA256),
 		giu.Label("Blake2b512"),
-		giu.InputText(&jsonOutput),
+		giu.InputText(&hashBlake2b512),
 	}
 }
--- ui/json.go
+++ ui/json.go
@@ -2,17 +2,35 @@ package ui
 
 import "github.com/AllenDang/giu"
 
-var (
-	jsonInput  string
-	jsonOutput string
-)
+var splitWidth float32 = 500
 
 func JSON() giu.Layout {
+	jsonInput := giu.CodeEditor().
+		ShowWhitespaces(false).
+		TabSize(2).
+		Text("select * from greeting\nwhere date > current_timestamp\norder by date").
+		Border(true)
+
+	jsonOutput := giu.CodeEditor().
+		ShowWhitespaces(false).
+		TabSize(2).
+		Text("").
+		Border(true)
+
 	return giu.Layout{
 		giu.Label("JSON Layout"),
-		giu.Label("Input"),
-		giu.InputText(&jsonInput),
-		giu.Label("Output"),
-		giu.InputText(&jsonOutput),
+		giu.Button("Format").OnClick(func() {
+			jsonOutput.Text(jsonInput.GetText())
+		}),
+		giu.SplitLayout(giu.DirectionVertical, &splitWidth,
+			giu.Layout{
+				giu.Label("Input"),
+				jsonInput.Text("input"),
+			},
+			giu.Layout{
+				giu.Label("Output"),
+				jsonOutput.Text("output"),
+			},
+		),
 	}
 }
